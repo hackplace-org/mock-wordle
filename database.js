85af1cb2690da4264aefc0a10e8770c3e9851f4a
Surya-Gedipudi <138071235+Surya-Gedipudi@users.noreply.github.com>
2023-10-06 19:53:30 -0400

    Add files via upload

---
+++ database.js
@@ -0,0 +1,106 @@
+let wordList = [ 
+    { word: "mango", 
+      hint: "tropical fruit sweet" 
+    }, 
+    { word: "candy", 
+      hint: "sweet sugary treat" 
+    }, 
+    { word: "azure", 
+      hint: "microsoft cloud platform" 
+    }, 
+    { word: "jpeg", 
+      hint: "image file format" 
+    }, 
+    { word: "pearl", 
+      hint: "precious organic gem" 
+    }, 
+    { word: "phone", 
+      hint: "mobile communication device" 
+    }, 
+    { word: "nvidia", 
+      hint: "gpu company brand" 
+    }, 
+    { word: "python", 
+      hint: "popular programming language" 
+    }, 
+    { word: "yahoo", 
+      hint: "web services company" 
+    }, 
+    { word: "tokyo", 
+      hint: "capital city japan" 
+    }, 
+    { word: "sheets", 
+      hint: "google spreadsheet product" 
+    }, 
+    { word: "dance", 
+      hint: "move to music" 
+    }, 
+    { word: "falcon", 
+      hint: "bird of prey" 
+    }, 
+    { word: "grape", 
+      hint: "small sweet fruit" 
+    }, 
+    { word: "honey", 
+      hint: "sweet syrup bee product" 
+    }, 
+    { word: "jelly", 
+      hint: "fruit spread jam" 
+    }, 
+    { word: "lemon", 
+      hint: "sour yellow citrus" 
+    }, 
+    { word: "melon", 
+      hint: "sweet watery fruit" 
+    }, 
+    { word: "olive", 
+      hint: "small fruit olive tree" 
+    }, 
+    { word: "peach", 
+      hint: "soft sweet fruit" 
+    }, 
+    { word: "queen", 
+      hint: "female royal ruler" 
+    }, 
+    { word: "bunny", 
+      hint: "small furry mammal" 
+    }, 
+    { word: "insect", 
+      hint: "small arthropod animal" 
+    }, 
+    { word: "lion", 
+      hint: "large wild cat" 
+    }, 
+    { word: "uncle", 
+      hint: "parent's male sibling" 
+    }, 
+    { word: "virus", 
+      hint: "microbe disease causing" 
+    }, 
+    { word: "water", 
+      hint: "clear odorless liquid" 
+    }, 
+    { word: "neon", 
+      hint: "chemical element gas" 
+    }, 
+    { word: "kayak", 
+      hint: "small narrow boat" 
+    }, 
+    { word: "bread", 
+      hint: "baked flour food" 
+    }, 
+    { word: "cheddar", 
+      hint: "hard cheese variety" 
+    }, 
+    { word: "llama", 
+      hint: "south american animal" 
+    }, 
+    { word: " cider", 
+      hint: "apple fermented drink" 
+    }, 
+    { word: "freezer", 
+      hint: "cold storage appliance" 
+    }, 
+    { word: "ukulele", 
+      hint: "small four string instrument" 
+    }, { word: "soccer", hint: "sport game with ball" }, { word: "igloo", hint: "snow house structure" }, { word: "jacket", hint: "outerwear clothing item" }, { word: "kettle", hint: "metal water boiling pot" }, { word: "foliage", hint: "leaves, branches, trees" }, { word: "praying", hint: "type of insect mantis" }, { word: "noodle", hint: "long thin pasta" }, { word: "orange", hint: "sweet citrus fruit" }, { word: "pepper", hint: "common spice and seasoning" }, { word: "mysql", hint: "database management system" }, { word: "iran", hint: "middle east country" }, { word: "flute", hint: "woodwind musical instrument" }, { word: "crypto", hint: "digital currencies technology" }, { word: "tesla", hint: "electric vehicle company" }, { word: "venus", hint: "planet in solar system" }, { word: "nginx", hint: "web server software" }, { word: "gmail", hint: "google email service" }, { word: "xhtml", hint: "markup language" }, { word: "helium", hint: "light nontoxic gas" }, { word: "notion", hint: "idea thought concept" }, { word: "azure", hint: "microsoft cloud platform" }, { word: "vector", hint: "mathematical diagram format" }, { word: "jpeg", hint: "image file format" }, { word: "search", hint: "act to find something" }, { word: "key", hint: "small piece of metal" }, { word: "egypt", hint: "country in north africa" }, { word: "joker", hint: "film about villain character" }, { word: "dubai", hint: "city in the uae" }, { word: "photo", hint: "picture image" }, { word: "congo", hint: "large river in africa" }, { word: "mist", hint: "fine water droplets air" }, { word: "rust", hint: "programming language" }, { word: "cello", hint: "large string instrument" }, { word: "focus", hint: "pay attention concentrate" }, { word: "earth", hint: "planet in space" }, { word: "silver", hint: "precious greyish metal" }, { word: "alibaba", hint: "chinese ecommerce site" }, { word: "scala", hint: "programming language jvm" }, { word: "coder", hint: "software developer" }, { word: "titan", hint: "science fiction movie series" }, { word: "issue", hint: "problem error bug" }, { word: "avatar", hint: "james cameron movie" }, { word: "spark", hint: "cluster computing framework" }, { word: "brainy", hint: "smart intelligent" }, { word: "atlas", hint: "book of maps" }, { word: "samoa", hint: "island country oceania" }, { word: "hockey", hint: "sport with stick and puck" }, { word: "chess", hint: "strategy board game" }, ]
